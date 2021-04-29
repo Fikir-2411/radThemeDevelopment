6cfa07b1f7db191f1d80cb97b88b17a6f6cff196
Fikir <fikirmuluken@gmail.com>
2021-04-29 08:47:18 +0300

    enqueue styles

--- functions.php
+++ functions.php
@@ -1,10 +1,13 @@
 <?php 
 	function tD_register_styles(){
-		wp_enqueue_style('themeDevelopment-bootstrap', get_template_directory_uri()."/style.css", array(), "1.0", 'all');
+		$version = wp_get_theme()->get('Version');
+		wp_enqueue_style('themeDevelopment-style', get_template_directory_uri()."/style.css", array(), $version, 'all');
+		wp_enqueue_style('themeDevelopment-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), "4.4.1", 'all');
+		wp_enqueue_style('themeDevelopment-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), "5.13.0", 'all');
 
 	}
 	
-	add_action()
+	add_action('wp_enqueue_scripts', 'tD_register_styles');
 
 
 ?>
