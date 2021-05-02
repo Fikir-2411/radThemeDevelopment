b51e862f458e6c51d10756c11a3713ea22ad4cd5
Amanuel Ewnetu <aewnetu6@gmail.com>
2021-05-02 10:30:32 +0300

    refactor

--- functions.php
+++ functions.php
@@ -1,4 +1,13 @@
 <?php 
+	
+	function tD_theme_support(){
+
+		add_theme_support('title-tag');
+	}
+
+	add_action('after_setup_theme', 'tD_theme_support')
+	
+	
 	function tD_register_styles(){
 		$version = wp_get_theme()->get('Version');
 		wp_enqueue_style('themeDevelopment-style', get_template_directory_uri()."/style.css", array('themeDevelopment-bootstrap'), $version, 'all');
--- header.php
+++ header.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en"> 
 <head>
-    <title>Blog Site Template</title>
     <!-- Meta -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -57,4 +56,8 @@
 
 			</div>
 		</nav>
-    </header>
+	</header>
+	<div class="main-wrapper">
+	    <header class="page-title theme-bg-light text-center gradient py-5">
+			<h1 class="heading">Blog Home Page Heading</h1>
+		</header>
