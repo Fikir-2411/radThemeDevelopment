da0db078d05bbf0f64d35f4429088836e85d0fb1
unknown <fasoking25@gmail.com>
2021-05-06 01:10:02 -1200

    edited

--- template-parts/content-article.php
+++ template-parts/content-article.php
@@ -6,7 +6,7 @@
             the_tags('<span class="tag"><i class="fa fa-tag"></i>','</spans><span class="tag"><i class="fa fa-tag"></i>','</span>');
          ?> 
         <span class="tag"><i class='fa fa-tag'></i>category</span><span class="comment"><a href="#comments"><i
-        class='fa fa-comment'></i> 3 comments</a></span>
+        class='fa fa-comment'></i><?php comments_number(); ?></a></span>
         </div>
         </header>
 <?php
