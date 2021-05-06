363bc8176ba2f6e6a8f53acd2431eb06ee70c3d7
unknown <fasoking25@gmail.com>
2021-05-06 01:24:43 -1200

    edited

--- comments.php
+++ comments.php
@@ -1,77 +1,164 @@
-<?php
-/**
- * The template for displaying comments
- *
- * This is the template that displays the area of the page that contains both the current comments
- * and the comment form.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package newTheme
- */
+<div class="comments-wrapper">
 
-/*
- * If the current post is protected by a password and
- * the visitor has not yet entered the password we will
- * return early without loading the comments.
- */
-if ( post_password_required() ) {
-	return;
-}
-?>
 
-<div id="comments" class="comments-area">
+					<div class="comments" id="comments">
 
-	<?php
-	// You can start editing here -- including this comment!
-	if ( have_comments() ) :
-		?>
-		<h2 class="comments-title">
-			<?php
-			$newtheme_comment_count = get_comments_number();
-			if ( '1' === $newtheme_comment_count ) {
-				printf(
-					/* translators: 1: title. */
-					esc_html__( 'One thought on &ldquo;%1$s&rdquo;', 'newtheme' ),
-					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
-				);
-			} else {
-				printf( 
-					/* translators: 1: comment count number, 2: title. */
-					esc_html( _nx( '%1$s thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', $newtheme_comment_count, 'comments title', 'newtheme' ) ),
-					number_format_i18n( $newtheme_comment_count ), // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-					'<span>' . wp_kses_post( get_the_title() ) . '</span>'
-				);
-			}
-			?>
-		</h2><!-- .comments-title -->
 
-		<?php the_comments_navigation(); ?>
+						<div class="comments-header">
 
-		<ol class="comment-list">
-			<?php
-			wp_list_comments(
-				array(
-					'style'      => 'ol',
-					'short_ping' => true,
-				)
-			);
-			?>
-		</ol><!-- .comment-list -->
+							<h2 class="comment-reply-title">
+								3 replies on “Hello world!” </h2><!-- .comments-title -->
 
-		<?php
-		the_comments_navigation();
+						</div><!-- .comments-header -->
 
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() ) :
-			?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'newtheme' ); ?></p>
-			<?php
-		endif;
+						<div class="comments-inner">
 
-	endif; // Check for have_comments().
+							<div id="comment-1" class="comment even thread-even depth-1 parent">
+								<article id="div-comment-1" class="comment-body">
+									<footer class="comment-meta">
+										<div class="comment-author vcard">
+											<a href="https://wordpress.org/" rel="external nofollow" class="url"><img
+													alt=""
+													src="http://1.gravatar.com/avatar/d7a973c7dab26985da5f961be7b74480?s=120&amp;d=mm&amp;r=g"
+													class="avatar avatar-120 photo" height="120" width="120"><span
+													class="fn">A WordPress Commenter</span><span
+													class="says sr-only">says:</span></a> </div>
+										<!-- .comment-author -->
 
-	comment_form();
-	?>
+										<div class="comment-metadata">
+											<a href="http://wordpress.mac/?p=1#comment-1">
+												<time datetime="2020-03-25T20:07:51+00:00"
+													title="March 25, 2020 at 8:07 pm">
+													March 25, 2020 at 8:07 pm </time>
+											</a>
+											
+										</div><!-- .comment-metadata -->
 
-</div><!-- #comments -->
+									</footer><!-- .comment-meta -->
+
+									<div class="comment-content entry-content">
+
+										<p>Hi, this is a comment.<br>
+											To get started with moderating, editing, and deleting comments, please visit
+											the Comments screen in the dashboard.<br>
+											Commenter avatars come from <a href="https://gravatar.com">Gravatar</a>.</p>
+
+									</div><!-- .comment-content -->
+
+
+									<footer class="comment-footer-meta">
+
+										<span class="comment-reply"><a rel="nofollow"
+												class="do-not-scroll comment-reply-link"
+												href="http://wordpress.mac/?p=1&amp;replytocom=1#respond"
+												data-commentid="1" data-postid="1" data-belowelement="div-comment-1"
+												data-respondelement="respond"
+												aria-label="Reply to A WordPress Commenter">Reply</a></span>
+									</footer>
+
+
+								</article><!-- .comment-body -->
+
+								<div id="comment-2"
+									class="comment byuser comment-author-andrew bypostauthor odd alt depth-2">
+									<article id="div-comment-2" class="comment-body">
+										<footer class="comment-meta">
+											<div class="comment-author vcard">
+												<img alt=""
+													src="http://2.gravatar.com/avatar/e297925dbb035adc0b67aa14da4dcc0a?s=120&amp;d=mm&amp;r=g"
+													class="avatar avatar-120 photo" height="120" width="120"><span
+													class="fn">andrew</span><span
+													class="says sr-only">says:</span> </div>
+											<!-- .comment-author -->
+
+											<div class="comment-metadata">
+												<a href="http://wordpress.mac/?p=1#comment-2">
+													<time datetime="2020-03-25T20:08:58+00:00"
+														title="March 25, 2020 at 8:08 pm">
+														March 25, 2020 at 8:08 pm </time>
+												</a>												
+											</div><!-- .comment-metadata -->
+
+										</footer><!-- .comment-meta -->
+
+										<div class="comment-content entry-content">
+
+											<p>test reply</p>
+
+										</div><!-- .comment-content -->
+
+
+										<footer class="comment-footer-meta">
+
+											<span class="comment-reply"><a rel="nofollow"
+													class="do-not-scroll comment-reply-link"
+													href="http://wordpress.mac/?p=1&amp;replytocom=2#respond"
+													data-commentid="2" data-postid="1" data-belowelement="div-comment-2"
+													data-respondelement="respond"
+													aria-label="Reply to andrew">Reply</a></span><span
+												class="by-post-author">By Post Author</span>
+										</footer>
+
+
+									</article><!-- .comment-body -->
+
+								</div><!-- #comment-## -->
+							</div><!-- #comment-## -->
+							<div id="comment-3"
+								class="comment byuser comment-author-andrew bypostauthor even thread-odd thread-alt depth-1">
+								<article id="div-comment-3" class="comment-body">
+									<footer class="comment-meta">
+										<div class="comment-author vcard">
+											<img alt=""
+												src="http://2.gravatar.com/avatar/e297925dbb035adc0b67aa14da4dcc0a?s=120&amp;d=mm&amp;r=g"
+												class="avatar avatar-120 photo" height="120" width="120"><span
+												class="fn">andrew</span><span
+												class="says sr-only">says:</span> </div>
+										<!-- .comment-author -->
+
+										<div class="comment-metadata">
+											<a href="http://wordpress.mac/?p=1#comment-3">
+												<time datetime="2020-03-25T20:09:31+00:00"
+													title="March 25, 2020 at 8:09 pm">
+													March 25, 2020 at 8:09 pm </time>
+											</a>
+											<span aria-hidden="true">•</span>
+										</div><!-- .comment-metadata -->
+
+									</footer><!-- .comment-meta -->
+
+									<div class="comment-content entry-content">
+
+										<p>Comment</p>
+
+									</div><!-- .comment-content -->
+
+
+									<footer class="comment-footer-meta">
+
+										<span class="comment-reply"><a rel="nofollow"
+												class="do-not-scroll comment-reply-link"
+												href="http://wordpress.mac/?p=1&amp;replytocom=3#respond"
+												data-commentid="3" data-postid="1" data-belowelement="div-comment-3"
+												data-respondelement="respond"
+												aria-label="Reply to andrew">Reply</a></span><span
+											class="by-post-author">By Post Author</span>
+									</footer>
+
+
+								</article><!-- .comment-body -->
+
+							</div><!-- #comment-## -->
+
+						</div><!-- .comments-inner -->
+
+					</div><!-- comments -->
+
+					<hr class="" aria-hidden="true">
+					<div id="respond" class="comment-respond">
+						<h2 id="reply-title" class="comment-reply-title">Leave a Reply <small><a rel="nofollow"
+									id="cancel-comment-reply-link" href="/?p=1#respond" style="display:none;">Cancel
+									reply</a></small></h2>
+					</div><!-- #respond -->
+
+				</div>
