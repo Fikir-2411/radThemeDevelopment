c548be3eccee201c54f2d9e05174bb00baec1611
nattysho <natnaelteferi15@gmail.com>
2021-05-11 10:12:16 -0800

    footer

--- footer.php
+++ footer.php
@@ -1,7 +1,11 @@
 <footer class="footer text-center py-2 theme-bg-dark">
 		   
             <p class="copyright"><a href="#">Personal website theme</a></p>
-		   
+
+			<?php
+                dynamic_sidebar('footer-1');
+		    ?>
+
 	    </footer>
 <?php 
 		wp_footer();//footer update
