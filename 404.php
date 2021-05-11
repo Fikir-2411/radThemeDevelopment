043dccafd95ed91b7fb86cfd2ed48f948e531615
nattysho <natnaelteferi15@gmail.com>
2021-05-11 10:19:44 -0800

    giving content

--- 404.php
+++ 404.php
@@ -2,17 +2,9 @@
 get_header();
 ?>
 <article class="content px-3 py-5 p-md-5">
-	<?php 
-		if( have_posts() ){
-			while( have_posts() ){
-				the_post();
-				get_template_part('template-parts/content','archive');
-			}
-		}
-	?>
-	<?php
-	the_posts_pagination(); 
-	?>
+	
+<h1>page not found</h1>
+
 </article>
 
 <?php 
