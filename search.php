10c8f78aaaa192827b51a2eb0960fa7e5479f193
nattysho <natnaelteferi15@gmail.com>
2021-05-12 09:29:23 -0800

    responsive search

--- search.php
+++ search.php
@@ -1,53 +1,18 @@
-<?php
-/**
- * The template for displaying search results pages
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
- *
- * @package newTheme
- */
 
+<?php
 get_header();
 ?>
-
-	<main id="primary" class="site-main">
-
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-					/* translators: %s: search query. */
-					printf( esc_html__( 'Search Results for: %s', 'newtheme' ), '<span>' . get_search_query() . '</span>' );
-					?>
-				</h1>
-			</header><!-- .page-header -->
-
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
+<article class="content px-3 py-5 p-md-5">
+	<?php 
+		if( have_posts() ){
+			while( have_posts() ){
 				the_post();
+				get_template_part('template-parts/content','archive');
+			}
+		}
+	?>
+</article>
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
-		?>
-
-	</main><!-- #main -->
-
-<?php
-get_sidebar();
+<?php 
 get_footer();
+?>
