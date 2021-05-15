c9f074bc5f54c61091db0389496e5f13c23c2fe6
Amanuel Ewnetu <aewnetu6@gmail.com>
2021-05-15 14:03:09 +0300

    refactored comments.php

--- comments.php
+++ comments.php
@@ -1,59 +1,40 @@
 <div class="comments-wrapper">
+	<div class="comments" id="comments">
+		<div class="comments-header">
+			<h4 class="comment-reply-title">
+			<?php
+				if(! have_comments()){
+					echo "Leave a Comment";
+				}
+				else{
+					echo get_comments_number()."comments";
+				}
+				?>
+		</h4>
+		</div>
+		<div class="comments-inner">
+		<?php
+			wp_list_comments(
+				array(
+					'avatar_size' => 50,
+					'style' => 'div',
+				)
+			);
+		?>
+		</div>
+	</div>
+	<hr class="" aria-hidden="true">
+	<?php
+		if(comments_open() ){
+			comment_form(
+				array(
+					'class_form' => '',
+					'title_reply_before' => '<h3 id="reply-title" class="comment-reply-title">',
+					'title_reply_after' => '</h3>'
+				)
 
+			);
+		}
 
-					<div class="comments" id="comments">
-
-
-						<div class="comments-header">
-
-							<h2 class="comment-reply-title">
-
-							<?php
-								if(! have_comments()){
-									echo "Leave a Comment";
-								}
-								else{
-									echo get_comments_number()."comments";
-								}
-								?>
-								
-						
-						</h2><!-- .comments-title -->
-
-						</div><!-- .comments-header -->
-
-						<div class="comments-inner">
-
-						<?php
-							wp_list_comments(
-								array(
-									'avatar_size' => 120,
-									'style' => 'div',
-								)
-					
-								);
-
-						?>
-
-							
-							
-						</div><!-- .comments-inner -->
-
-					</div><!-- comments -->
-
-					<hr class="" aria-hidden="true">
-					<?php
-						if(comments_open() ){
-							comment_form(
-								array(
-									'class_form' => ''
-									'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
-									'title_reply_after' => '</h2>'
-
-								);
-
-							);
-						}
-
-					?>
-				</div>
+	?>
+</div>
