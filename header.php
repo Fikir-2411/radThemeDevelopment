14a387dee5fbaae490b4033da4163fa54a325059
Amanuel Ewnetu <aewnetu6@gmail.com>
2021-05-15 16:10:40 +0300

    style.css

--- header.php
+++ header.php
@@ -17,39 +17,42 @@
 <body>
     
     <header class="header">	    
-	    <a class="site-title pt-lg-4 mb-0" href="index.html">
+	    <!-- <a class="site-title pt-lg-4 mb-0" href="index.html">
+			
+		</a> -->
+		
+        
+	    <nav class="navbar navbar-expand-lg navbar-light" >
+		<a class="navbar-brand" href="index.html">
+			<?php 
+				if(function_exists('the_custom_logo')){
+					$custom_logo_id = get_theme_mod('custom_logo');
+					$logo = wp_get_attachment_image_src($custom_logo_id);
+				}		
+			?>	
+			<img class="rounded-circle" width="50" height="50" src="<?php echo $logo[0] ?>" alt="logo" >			
 			<?php echo get_bloginfo( 'name' ); ?>
 		</a>
-        
-	    <nav class="navbar navbar-expand-lg navbar-dark" >
            
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-			</button>
-			<div id="navigation" class="collapse navbar-collapse" >
-				
-				<?php 
-					if(function_exists('the_custom_logo')){
-						$custom_logo_id = get_theme_mod('custom_logo');
-						$logo = wp_get_attachment_image_src($custom_logo_id);
-					}		
-				?>	
-				<img class="d-inline-block align-top rounded-img" width="50" height="50" src="<?php echo $logo[0] ?>" alt="logo" >			
-				<?php 
-					wp_nav_menu(
-						array(
-							'menu' => 'primary',
-							'container' => '',
-							'theme_location' => 'primary',
-							'items_wrap' => '<ul id="" class="nav justify-content-end">%3$s</ul>'
-						)
-					);
-				?>
-				<hr>
-			</div>
-		</nav>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+		</button>
+		<div id="navigation" class="collapse navbar-collapse" >			
+			<?php 
+				wp_nav_menu(
+					array(
+						'menu' => 'primary',
+						'container' => '',
+						'theme_location' => 'primary',
+						'items_wrap' => '<ul id="" class="navbar-nav">%3$s</ul>'
+					)
+				);
+			?>
+			<hr>
+		</div>
+	</nav>
+</header>
+<div class="main-wrapper">
+	<header class="page-title theme-bg-light text-center gradient py-5">
+		<h1 class="heading"><?php the_title(); ?></h1>
 	</header>
-	<div class="main-wrapper">
-	    <header class="page-title theme-bg-light text-center gradient py-5">
-			<h1 class="heading"><?php the_title(); ?></h1>
-		</header>
