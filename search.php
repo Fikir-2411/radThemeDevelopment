ca4c5c2b7a44cbfaf9df44b1563b43ed0f2132f4
nattysho <natnaelteferi15@gmail.com>
2021-05-26 06:34:17 -0800

    creating header

--- search.php
+++ search.php
@@ -1,18 +1,3 @@
+<?php get_header() ?>
 
-<?php
-get_header();
-?>
-<article class="content px-3 py-5 p-md-5">
-	<?php 
-		if( have_posts() ){
-			while( have_posts() ){
-				the_post();
-				get_template_part('template-parts/content','archive');
-			}
-		}
-	?>
-</article>
-
-<?php 
-get_footer();
-?>
+<main class="container-fluid py-5">
