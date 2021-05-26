14d566b79ccd31b2f623415536ecac749b3e3029
Fikir <fikirmuluken@gmail.com>
2021-05-26 17:46:01 +0300

    instantiate 404

--- 404.php
+++ 404.php
@@ -1,17 +1,10 @@
 <?php
 get_header();
 ?>
-<article class="content px-3 py-5 p-md-5">
-	
-<h1>page not found</h1>
+
+
+
 <?php
-
-get_search_form();
-
-?>
-
-</article>
-
-<?php 
+get_sidebar();
 get_footer();
-?>
+
